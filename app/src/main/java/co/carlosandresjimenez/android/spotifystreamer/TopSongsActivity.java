61d52a78b3d73da058083c017a4eb158c0eb4509
Carlos Andrés Jiménez <crlsndrsjmnz@gmail.com>
2015-08-19 15:43:10 +0200

    Stage 2, Task 1 implemented: Master Detail Flow implemented

--- app/src/main/java/co/carlosandresjimenez/android/spotifystreamer/TopSongsActivity.java
+++ app/src/main/java/co/carlosandresjimenez/android/spotifystreamer/TopSongsActivity.java
@@ -1,12 +1,9 @@
 package co.carlosandresjimenez.android.spotifystreamer;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by carlosjimenez on 7/7/15.
- */
-public class TopSongsActivity extends ActionBarActivity {
+public class TopSongsActivity extends AppCompatActivity {
 
     private final String LOG_TAG = TopSongsActivity.class.getSimpleName();
 
@@ -14,8 +11,25 @@ public class TopSongsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
 
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+
+            Bundle arguments = new Bundle();
+            arguments.putString(TopSongsFragment.KEY_ARTIST_ID,
+                    getIntent().getStringExtra(TopSongsFragment.KEY_ARTIST_ID));
+            arguments.putString(TopSongsFragment.KEY_ARTIST_NAME,
+                    getIntent().getStringExtra(TopSongsFragment.KEY_ARTIST_NAME));
+
+            TopSongsFragment fragment = new TopSongsFragment();
+            fragment.setArguments(arguments);
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.artist_detail_container, fragment)
+                    .commit();
+        }
+    }
 
 
 }
